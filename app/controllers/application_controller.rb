0c9b45df572e085d7f84e3e9bf0203da32733398
Paul <poshea48@msn.com>
2018-08-30 20:50:30 +0000

    Handle article not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+  
+  protected
+  
+  def resource_not_found
+    flash[:danger] = "That path does not exist"
+    redirect_to root_path 
+  end
 end
